13dc19c77e488d307dec3bb64b1f5e69794c9dfb
David Anderson <matandersonlv@hotmail.com>
2018-12-20 05:44:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,31 @@
 # code here!
 class School
-  attr_accessor :roster, :student_name, :school_name
+
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
 
-  def add_student(student_name, grade)
-  if @roster[grade]
-    @roster[grade] << student_name
-  else
-    @roster[grade] = []
-    @roster[grade] << student_name
+  def roster
+    @roster
   end
-end
 
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
