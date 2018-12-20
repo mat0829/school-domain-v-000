b0cd6196011b1bacdd9b83cb2caa8ce971b9ec15
David Anderson <matandersonlv@hotmail.com>
2018-12-20 05:35:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,4 +5,14 @@ class School
     @school_name = school_name
     @roster = {}
   end
+
+  def add_student(student_name, grade)
+    if @roster[grade] == true
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+  
 end
